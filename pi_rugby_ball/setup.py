1bde75e976de2bb8a2ff0c2e2f02749a9dee3743
Andy Blight <a.j.blight@leeds.ac.uk>
2023-12-03 11:53:27 +0000

    colcon build works.  Launch file runs.

--- pi_rugby_ball/setup.py
+++ pi_rugby_ball/setup.py
@@ -5,13 +5,11 @@ from setuptools import setup
 package_name = 'pi_rugby_ball'
 
 setup(
-    name=package_name,
     version='0.0.1',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
         (
             os.path.join("share", package_name + "/launch"),
             glob("launch/*launch.[pxy][yma]*"),
