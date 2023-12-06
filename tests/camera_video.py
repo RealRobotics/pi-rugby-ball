af6b8a255e3e4671910aafd7e88d3e14b963671b
Andy Blight <a.j.blight@leeds.ac.uk>
2023-12-06 11:34:49 +0000

    Updated camera video script to use HD

--- tests/camera_video.py
+++ tests/camera_video.py
@@ -8,8 +8,10 @@ from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 
 picam2 = Picamera2()
-video_config = picam2.create_video_configuration()
-picam2.configure(video_config)
+# video_config = picam2.create_video_configuration()
+# picam2.configure(video_config)
+video_config_1080 = picam2.video_configuration({"size": (1920, 1080)}, raw={"size": (1640, 1232)}, controls={"FrameDurationLimits": (33333, 33333)})
+picam2.configure(video_config_1080)
 
 encoder = H264Encoder(10000000)
 
