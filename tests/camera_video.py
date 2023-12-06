20ef029a3c890d9c859652f2547dd4b3d6dc7215
Andy Blight <a.j.blight@leeds.ac.uk>
2023-12-06 11:27:10 +0000

    Updated camera video script

--- tests/camera_video.py
+++ tests/camera_video.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
+# Copied from
+# https://github.com/raspberrypi/picamera2/blob/main/examples/capture_video.py
+
 import time
 
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
-from picamera2.outputs import FfmpegOutput
 
 picam2 = Picamera2()
 video_config = picam2.create_video_configuration()
 picam2.configure(video_config)
 
 encoder = H264Encoder(10000000)
-output = FfmpegOutput('test.mp4')
 
-picam2.start_recording(encoder, output)
+picam2.start_recording(encoder, 'test.h264')
 time.sleep(10)
 picam2.stop_recording()
